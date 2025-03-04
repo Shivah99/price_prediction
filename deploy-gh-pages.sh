9f36b9974748172b90d263ff600dc6325c46fe97
Shivah99 <shivaji9712@gmail.com>
2025-03-03 19:31:56 -0500

    Configure GitHub Pages deployment

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,9 @@
+# Install gh-pages package if you haven't already
+npm install --save-dev gh-pages
+
+# Add these scripts to your package.json
+# "predeploy": "npm run build",
+# "deploy": "gh-pages -d build"
+
+# Then run:
+npm run deploy
